d37edb4b7de9b5c655abede33cec50f28805d03a
Neil Guan <guan840912@gmail.com>
2020-08-27 13:35:23 +0800

    add some modify test/index.api.ts

--- test/integ.api.ts
+++ test/integ.api.ts
@@ -1,5 +1,5 @@
-import { GitlabContainerRunner } from '../lib/index';
-import { App, Stack, CfnOutput } from '@aws-cdk/core';
+import { GitlabContainerRunner, BlockDuration } from '../lib/index';
+import { App, Stack, Duration, CfnOutput } from '@aws-cdk/core';
 import { Port, Peer } from '@aws-cdk/aws-ec2';
 import { ManagedPolicy, Role, ServicePrincipal } from '@aws-cdk/aws-iam';
 
@@ -11,16 +11,24 @@ const role = new Role(stack, 'runner-role', {
   roleName: 'TestRole',
 });
 const runner = new GitlabContainerRunner(stack, 'testing', {
-  gitlabtoken: 'GITLAB_TOKEN',
-  ec2type: 't3.small',
+  gitlabtoken: 'akjvr3qz72Hdw8fZNb8T',
+  ec2type: 't3.large',
   ec2iamrole: role,
-  gitlaburl: 'https://gitlab.com/',
   ebsSize: 100,
+  blockDuration: BlockDuration.ONE_HOUR,
+  spotFleet: true,
 });
+
+runner.expireAfter(Duration.hours(1));
 runner.runnerRole.addManagedPolicy(
   ManagedPolicy.fromAwsManagedPolicyName('AmazonS3ReadOnlyAccess'),
 );
-runner.runnerEc2.connections.allowFrom(Peer.ipv4('0.0.0.0/0'), Port.tcp(80));
-runner.runnerEc2.connections.allowFrom(Peer.ipv4('0.0.0.0/0'), Port.tcp(443));
+runner.defaultRunnerSG.connections.allowFromAnyIpv4(Port.tcp(80));
+runner.defaultRunnerSG.connections.allowFrom(
+  Peer.ipv4('0.0.0.0/0'),
+  Port.tcp(443),
+);
+
+//runner.runnerEc2.connections.allowFrom(Peer.ipv4('0.0.0.0/0'), Port.tcp(8080));
 new CfnOutput(stack, 'role', { value: runner.runnerRole.roleArn });
-new CfnOutput(stack, 'InstanceID', { value: runner.runnerEc2.instanceId });
+//new CfnOutput(stack, 'InstanceID', { value: runner.runnerEc2.instanceId });
