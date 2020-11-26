c258aca9d5acae99b6ab2ceedaf5f60258b16814
Neil Kuan <guan840912@gmail.com>
2020-11-26 12:28:20 +0800

    fix: projen version issue
    feat: dockerVolumes
    
    update aws cdk version to 1.75.0

---
+++ src/gitlab-runner-interfaces.ts
@@ -0,0 +1,17 @@
+export interface DockerVolumes {
+  /**
+   * EC2 Runner Host Path
+   *
+   * @example - /tmp/cahce
+   * more detail see https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-runnersdocker-section
+   */
+  readonly hostPath: string;
+
+  /**
+   * Job Runtime Container Path Host Path
+   *
+   *  @example - /tmp/cahce
+   * more detail see https://docs.gitlab.com/runner/configuration/advanced-configuration.html#the-runnersdocker-section
+   */
+  readonly containerPath: string;
+}
