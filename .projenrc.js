8280e90d8ff396fda82a988d83f3211c1ebaff5f
Neil Kuan <guan840912@gmail.com>
2022-06-06 16:10:10 +0800

    chore: resolutions @types/prettier

--- .projenrc.js
+++ .projenrc.js
@@ -54,6 +54,7 @@ project.package.addField('resolutions', {
   'ansi-regex': '^5.0.1',
   '@babel/core': '7.16.12',
   'minimist': '^1.2.5',
+  '@types/prettier': '2.6.0',
 });
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage', 'venv'];
 project.gitignore.exclude(...common_exclude);
