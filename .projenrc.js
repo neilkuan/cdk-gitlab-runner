4956cf2795f9cb58f49ae99095eaab7a8e1057ab
neilkuan <guan840912@gmail.com>
2023-07-04 08:57:33 +0000

    chore: deps upgrade

--- .projenrc.js
+++ .projenrc.js
@@ -17,7 +17,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
   },
   compat: true,
   stability: 'experimental',
-  cdkVersion: '1.176.0',
+  cdkVersion: '1.204.0',
   /**
    * we default release the main branch(cdkv2) with major version 2.
    */
