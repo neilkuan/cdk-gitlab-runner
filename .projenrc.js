159fb74595ca30d2332f9e438e0996b6c9c8672e
Neil Kuan <46012524+neilkuan@users.noreply.github.com>
2026-02-28 04:14:34 +0000

    chore: upgrade node version

--- .projenrc.js
+++ .projenrc.js
@@ -62,10 +62,10 @@ const project = new awscdk.AwsCdkConstructLibrary({
     'ts-jest@29.1.2',
     'jsii-rosetta@5.0.x',
   ],
-  minNodeVersion: '20.10.0',
-  workflowNodeVersion: '20.10.0',
+  minNodeVersion: '^24',
+  workflowNodeVersion: '^24',
   typescriptVersion: '^5',
-  jsiiVersion: '5.3.x',
+  jsiiVersion: '5.9.x',
 });
 const common_exclude = ['cdk.out', 'cdk.context.json', 'yarn-error.log', 'coverage', 'venv'];
 project.gitignore.exclude(...common_exclude);
